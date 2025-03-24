abf025e90102227000703be046ea7e8eab873347
Conor Giffen-McCloskey <conorgiffenmccloskey@gmail.com>
2025-03-24 00:37:20 +0000

    feat: Integrate custom functionality for expense entries in main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import { login } from './requests/login.js';
 import { getVehicles, filterVehicles } from './requests/vehicleEntries.js';
 import { getFuellingEntries } from './requests/fuellingEntries.js';
 import { getServicingEntries } from './requests/servicingEntries.js';
+import {
+  addExpenseEntries,
+  deleteExpenseEntries,
+} from './requests/expenseEntries.js';
 import { args } from './config/config.js';
 
 async function main() {
@@ -19,9 +23,18 @@ async function main() {
   const vehicles = await getVehicles(authToken);
   const chosenVehicleId = filterVehicles(vehicles);
 
+  if (args.custom) {
+    console.log('custom functionality has been enabled.');
+    // await addExpenseEntries(authToken, chosenVehicleId);
+    // await deleteExpenseEntries(authToken, chosenVehicleId);
+    return;
+  }
+
   if (chosenVehicleId) {
     await getFuellingEntries(authToken, chosenVehicleId);
     await getServicingEntries(authToken, chosenVehicleId);
+    // should results be returned from these functions instead
+    // and then saved to a file here?
   }
 }
 
